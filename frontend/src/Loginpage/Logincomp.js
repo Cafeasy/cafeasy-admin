0958f53e2627c60cc1efe19da0fe7e095497d685
muhamadadam2001 <61117916+muhamadadam2001@users.noreply.github.com>
2023-06-06 15:33:43 +0700

    login comp & login page

---
+++ frontend/src/Loginpage/Logincomp.js
@@ -0,0 +1,11 @@
+import logodannama from "../Photos/logodannama.png";
+import "../Loginpage/Loginpage.css";
+
+function Logincomp() {
+    return (
+        <>
+            <h3>HOMEPAGE</h3>
+        </>
+    )
+}
+export default Logincomp;
