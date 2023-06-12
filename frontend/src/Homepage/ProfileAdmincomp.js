88639f983698206e2cfa01e37759135db63cfb40
muhamadadam2001 <61117916+admnsonn@users.noreply.github.com>
2023-06-13 04:59:26 +0700

    styling sidebar

--- frontend/src/Homepage/ProfileAdmincomp.js
+++ frontend/src/Homepage/ProfileAdmincomp.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import '../Crud/Crud.css';
 
-const Pembukuancomp = () => {
+const Pembukuancomp = (value) => {
   const [users, setUser] = useState([]);
 
 // useEffect(() => {
--- frontend/src/Homepage/Sidebardata.js
+++ frontend/src/Homepage/Sidebardata.js
@@ -1,37 +1,38 @@
 import React from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as RiIcons from 'react-icons/ri';
 import * as MdIcons from 'react-icons/md';
+import * as CgIcons from 'react-icons/cg';
+import * as AiIcons from 'react-icons/ai';
+import * as FaIcons from 'react-icons/fa';
 
 export const Sidebardata = [
   {
     title: 'Profile Admin',
     path: '/ProfileAdmin',
-    icon: <FaIcons.FaUserEdit />,
+    icon: <CgIcons.CgProfile />,
     cName: 'nav-text'
   },
   {
     title: 'Data Menu',
     path: '/DataMenu',
-    icon: <FaIcons.FaUserEdit />,
+    icon: <MdIcons.MdMenuBook />,
     cName: 'nav-text'
   },
   {
     title: 'Data Transaksi',
     path: '/DataTransaksi',
-    icon: <RiIcons.RiFileUploadFill />,
+    icon: <AiIcons.AiOutlineTransaction />,
     cName: 'nav-text'
   },
   {
     title: 'Data Pelanggan',
     path: '/DataPelanggan',
-    icon: <MdIcons.MdOutlineInventory />,
+    icon: <AiIcons.AiFillCustomerService />,
     cName: 'nav-text'
   },
   {
     title: 'Data Riwayat Transaksi',
     path: '/DataRiwayat',
-    icon: <MdIcons.MdOutlineInventory />,
+    icon: <FaIcons.FaHandshake />,
     cName: 'nav-text'
   },
 ];
